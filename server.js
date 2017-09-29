76cdca5d23ebfa53b279e9365801ab4b083b4338
cristiano.gonzaga <cristianogonzaga.si@gmail.com>
2017-09-29 13:58:39 -0300

    ajuste estrutura

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = require('./config/express');
 
 http
   .createServer(app)
-  .listen(8081, function(){
-    console.log('Serviço iniciado na porta 8081');
+  .listen(8080, function(){
+    console.log('Serviço iniciado na porta 8080');
   });
